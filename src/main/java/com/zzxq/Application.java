2881db183a6ac04e54c0468e5d2e5fa5bdaab5a1
zzx <1847606108@qq.com>
2022-09-16 09:46:42 +0800

    third commit

--- src/main/java/com/zzxq/Application.java
+++ src/main/java/com/zzxq/Application.java
@@ -19,5 +19,6 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
         System.out.println(1);
+        System.out.println(1);
     }
 }
